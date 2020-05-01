f5249e58d313f346eb37e67be27afb14f2dd506e
gonsuke777 <black_tiger777@yahoo.co.jp>
2020-05-01 23:03:14 +0900

    Specific export AD delete.

--- oci_test_env1/availability_domain.tf
+++ oci_test_env1/availability_domain.tf
@@ -1,10 +1,5 @@
 # Avilability Domain
 
-data oci_identity_availability_domain export_xCoU-AP-TOKYO-1-AD-1 {
-  compartment_id = "${var.compartment_ocid}"
-  ad_number      = "1"
-}
-
 # Get a list of Availability Domains, this code from https://www.terraform.io/docs/providers/oci/guides/best_practices.html
 data "oci_identity_availability_domains" "ads" {
   compartment_id = "${var.tenancy_ocid}"
@@ -14,5 +9,4 @@ data "oci_identity_availability_domains" "ads" {
 output "show-ads" {
   value = "${data.oci_identity_availability_domains.ads.availability_domains}"
 }
-
 
