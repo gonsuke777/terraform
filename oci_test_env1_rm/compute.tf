67dfadb47c51ba75aafea43ecba1e56248850d78
gonsuke777 <black_tiger777@yahoo.co.jp>
2026-05-20 20:19:25 +0900

    compute.tf を修正
    ・Cloud Init で kernel を Update するように修正
    ・are_legacy_imds_endpoints_disabled = true を追記(無いとエラー)

--- oci_test_env1_rm/compute.tf
+++ oci_test_env1_rm/compute.tf
@@ -1,5 +1,20 @@
 # Compute Instance
 
+locals {
+  kernel_update_user_data = <<-EOT
+#cloud-config
+runcmd:
+  - |
+    set -e
+    if command -v dnf >/dev/null 2>&1; then
+      dnf -y update 'kernel*'
+    else
+      yum -y update 'kernel*'
+    fi
+    shutdown -r +1 "Rebooting to apply kernel update"
+EOT
+}
+
 resource oci_core_instance export_BASTION1 {
   compartment_id      = "${var.compartment_ocid}"
   display_name        = "${upper(var.oci_resource_prefix)}-BASTION1"
@@ -15,6 +30,9 @@ resource oci_core_instance export_BASTION1 {
     source_type = "image"
     source_id   = "${var.os_image_source_id}"
   }
+  instance_options {
+    are_legacy_imds_endpoints_disabled = true
+  }
   create_vnic_details {
     assign_public_ip = "true"
     hostname_label   = "${lower(var.oci_resource_prefix)}-bastion1"
@@ -22,6 +40,7 @@ resource oci_core_instance export_BASTION1 {
   }
   metadata = {
     ssh_authorized_keys = "${file(var.public_compute_ssh_key)}"
+    user_data           = "${base64encode(local.kernel_update_user_data)}"
   }
 }
 
@@ -40,6 +59,9 @@ resource oci_core_instance export_PRVCOMP1 {
     source_type = "image"
     source_id   = "${var.os_image_source_id}"
   }
+  instance_options {
+    are_legacy_imds_endpoints_disabled = true
+  }
   create_vnic_details {
     assign_public_ip = "false"
     hostname_label   = "${lower(var.oci_resource_prefix)}-prvcomp1"
@@ -47,6 +69,7 @@ resource oci_core_instance export_PRVCOMP1 {
   }
   metadata = {
     ssh_authorized_keys = "${file(var.private_compute_ssh_key)}"
+    user_data           = "${base64encode(local.kernel_update_user_data)}"
   }
 }
 
